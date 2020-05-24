62834717d093a5d148b3856d934695814469ab7c
Bogdan Kasich <44712899+Strigalet@users.noreply.github.com>
2020-05-25 02:55:31 +0300

    Update run.sql

--- run.sql
+++ run.sql
@@ -1,11 +1,6 @@
 SET SERVEROUTPUT ON;
 
 
- 
-DECLARE
- 
-counter INT;
- 
 CURSOR Participant_Solution
 IS
     SELECT
@@ -39,6 +34,11 @@ BEGIN
      dbms_output.put_line('Function Result:');
      SELECT * FROM TABLE(Participants_Avg_Mark_By_Year_And_Country(1991,'RUS'))
      SELECT * FROM TABLE(Participants_Avg_Mark_By_Year_And_Country(2012,'USA'))
+     
+     
+     dbms_output.put_line(' ');
+     dbms_output.put_line('Trigger Reaction:');
+     INSERT INTO Participation(year,participant_id) VALUES (2032, 1);
      
         
 END;
