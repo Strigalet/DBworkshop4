5f43fbbb17c5bed248e7627fc4a959dd4744b9b9
Bogdan Kasich <44712899+Strigalet@users.noreply.github.com>
2020-05-25 02:15:55 +0300

    Update function.sql

--- function.sql
+++ function.sql
@@ -1,16 +1,36 @@
-CREATE OR REPLACE FUNCTION HigherAvgMarkByYearAndCountry (
-    par_year        participation.year%TYPE,
-    par_countryname   participant.countryname%TYPE
-) RETURN INT
-AS
-    counter INT;
-BEGIN 
-    SELECT COUNT(average_mark) 
-    INTO counter 
-    FROM Country_Mark_By_Year 
-    WHERE year = par_year AND
-          countryname = par_countryname AND
-          average_mark >= 4;
+CREATE TYPE AveragePeople IS OBJECT (
+        firstname   VARCHAR(85),
+        lastname    VARCHAR(85),
+        avg_mark    INT
+    );
+/
+CREATE TYPE TableAveragePeople IS
+        TABLE OF rowprojects;
+/
+CREATE OR REPLACE FUNCTION Participants_Avg_Mark_By_Year_And_Country (
+        par_year    NUMBER(4,0),
+        par_countryname    VARCHAR
+    ) RETURN TableAveragePeople
+        PIPELINED
+    IS
 
-    RETURN counter;
-END;
+        CURSOR curs IS
+        (SELECT
+            firstname,
+            lastname,
+            avg_mark
+        FROM
+            Participants_Avg_Mark
+        WHERE
+            countryname = par_countryname
+            AND year = par_year);
+    my_rec rowprojects;
+    BEGIN
+        FOR rec IN curs 
+        LOOP
+            my_rec.firstname := rec.firstname;
+            my_rec.lastname := rec.lastname;
+            my_rec.avg_mark := rec.avg_mark;
+            PIPE ROW ( my_rec );
+        END LOOP;
+    END;
