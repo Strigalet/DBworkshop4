f9a04aff41f54aa877b55d65bf1bcebc23deca74
Bogdan Kasich <44712899+Strigalet@users.noreply.github.com>
2020-05-24 20:49:44 +0300

    Update view.sql

--- view.sql
+++ view.sql
@@ -1,12 +1,11 @@
 CREATE OR REPLACE VIEW Country_Mark_By_Year AS
 SELECT
    countryname,
-   mark,
+   sum(mark)/count(mark) average_mark,
    year
 FROM Participant 
 		JOIN Solution ON 
 			Participant.participant_id = Solution.participant_id
 		JOIN Participation ON
 			Participant.participant_id = Participation.participant_id
-
-			
+GROUP BY countryname, year;
