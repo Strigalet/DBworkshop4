649bcbc8f1f31276bc8f47d8da8fb269b77dc48c
Bogdan Kasich <44712899+Strigalet@users.noreply.github.com>
2020-05-24 20:29:21 +0300

    Update procedure.sql

--- procedure.sql
+++ procedure.sql
@@ -21,24 +21,24 @@ CREATE OR REPLACE PROCEDURE Add_Participant_Solution (
 	BEGIN
 		
 		SELECT COUNT(*)	INTO var_participant_id
-    	FROM participant
-    	WHERE participant_id = par_participant_id;
+    		FROM participant
+    		WHERE participant_id = par_participant_id;
     
-    	SELECT COUNT(*)	INTO var_problem_id
-    	FROM problem
-    	WHERE problem_id = par_problem_id;
+    		SELECT COUNT(*)	INTO var_problem_id
+    		FROM problem
+    		WHERE problem_id = par_problem_id;
 
 
-    	IF (var_participant_id = 1)  and (var_problem_id = 1) THEN
-    		INSERT INTO solution (solution, participant_id, problem_id, mark) VALUES (par_solution, par_participant_id, par_problem_id, par_mark);
+    		IF (var_participant_id = 1)  and (var_problem_id = 1) THEN
+    			INSERT INTO solution (solution, participant_id, problem_id, mark) VALUES (par_solution, par_participant_id, par_problem_id, par_mark);
 
-    	ELSE
-    		RAISE problem_or_participant_not_found;
+    		ELSE
+    			RAISE problem_or_participant_not_found;
     		
-    	END IF;
+    		END IF;
 
-    	EXCEPTION
-    		WHEN problem_or_participant_not_found THEN
-    			dbms_output.put_line('Participant or problem not found');
+    		EXCEPTION
+    			WHEN problem_or_participant_not_found THEN
+    				dbms_output.put_line('Participant or problem not found');
 
 END;
