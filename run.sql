d78ea6a1774274cc3cf1f21d9aaa09a79fa21e09
Bogdan Kasich <44712899+Strigalet@users.noreply.github.com>
2020-05-25 17:56:03 +0300

    Update run.sql

--- run.sql
+++ run.sql
@@ -1,4 +1,14 @@
 SET SERVEROUTPUT ON;
+--ПОЖАЛУЙСТА ЗАПУСТИТЕ ФАЙЛ package.sql
+
+
+SELECT * FROM TABLE(my_package.GetAvgMark(1991,'RUS'));           -- Работоспособность функции
+SELECT * FROM TABLE(my_package.GetAvgMark(2012,'USA'));
+     
+     
+    
+INSERT INTO Participation(year,participant_id) VALUES (2032, 1);           -- Работоспособность триггера
+
 
 DECLARE
 CURSOR Participant_Solution
@@ -23,22 +33,14 @@ BEGIN
     dbms_output.put_line('AFTER:');
     FOR record IN Participant_Solution
      LOOP
-			
         DBMS_OUTPUT.put_line('Participant_id - ' || record.participant_id || ' | Problem_id - ' || record.problem_id || ' | Solution - ' || record.solution);
      END LOOP;
-     
+     dbms_output.put_line(' ');
+     dbms_output.put_line('Procedure exceptions: ');
      Add_Participant_Solution(15,2,'yareshil');  	--Participant not found
      Add_Participant_Solution(2,15,'solutionenko');	--Problem not found
      
-     dbms_output.put_line(' ');
-     dbms_output.put_line('Function Result:');
-     SELECT * FROM TABLE(my_package.GetAvgMark(1991,'RUS'));
-     SELECT * FROM TABLE(my_package.GetAvgMark(2012,'USA'));
      
-     
-     dbms_output.put_line(' ');
-     dbms_output.put_line('Trigger Reaction:');
-     INSERT INTO Participation(year,participant_id) VALUES (2032, 1);
      
         
 END;
